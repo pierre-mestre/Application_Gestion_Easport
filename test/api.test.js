a17303af5490225b2ffcb5cc94b6acf1b49ad102
pierre-mestre <42938376+pierre-mestre@users.noreply.github.com>
2018-12-10 14:05:30 +0100

    Reparation des tests

--- test/api.test.js
+++ test/api.test.js
@@ -1,8 +1,8 @@
 import request from "supertest";
 import app from "../server/app";
 
-describe("Test the post-example", () => {
-  test("It should return what is sent", async () => {
+describe("Test the post-login", () => {
+  test("It should return the logged page", async () => {
     const expectedJson = {
       pseudo: "Eliot",
       password: "test"
@@ -12,16 +12,6 @@ describe("Test the post-example", () => {
       .post("/post-login")
       .send(expectedJson);
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(`<form>
-                <div class="form-group">
-                <label for="username">Nom d'utilisateur</label>
-                <input type="text" class="form-control" id="username" placeholder="Pseudo" value="${req.body.pseudo}">
-                </div>
-                <div class="form-group">
-                <label for="password">Mot de passe</label>
-                <input type="password" class="form-control" id="password">
-                </div>
-                <button id="login-submit" type="button" class="btn btn-primary">Connexion</button>
-              </form>`);
+    expect(response.body).toEqual('<p>Bienvenue</p>');
   });
 });
