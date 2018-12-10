c9946833bb49050ce62892ae9011f0a42b9c1503
pierre-mestre <42938376+pierre-mestre@users.noreply.github.com>
2018-12-10 16:32:05 +0100

    use env variable port for node

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import app from "./app";
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000!");
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log(`Server started on port ${port}`);
 });
