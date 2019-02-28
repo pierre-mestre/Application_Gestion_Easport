1b157e2abaad80cdddb7c65b92afe9ec5b2f5ae9
pierre-mestre <42938376+pierre-mestre@users.noreply.github.com>
2019-02-28 12:58:01 +0100

    try to fix loginPost.js w/ removing req.session.* values

--- pages/login/loginPost.js
+++ pages/login/loginPost.js
@@ -1,17 +1,16 @@
 module.exports = {
 	post_login: function(req, res, dao){
-		/*dao.getUserBySignIn(req.body.email, req.body.password, function(result) {
+		dao.getUserBySignIn(req.body.email, req.body.password, function(result) {
 			if(result.rowCount == 0) req.body.connected = false;
 			else {
 				console.log(result.rows[0].id);
-				req.session.mail = result.rows[0].mail;
+				/*req.session.mail = result.rows[0].mail;
 				req.session.id = result.rows[0].id;
-				req.session.pseudo = result.rows[0].pseudo;
+				req.session.pseudo = result.rows[0].pseudo;*/
 				req.body.connected = true;
 			}
 			res.set("Content-Type", "application/json");
 			res.send(req.body);
-		});*/
-		res.redirect('/');
+		});
 	}
 }
