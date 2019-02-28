c4ccc4369c72f3f012d622668260d433504cabbd
pierre-mestre <42938376+pierre-mestre@users.noreply.github.com>
2019-02-28 14:11:20 +0100

    update api to my profil

--- pages/profile/profilePost.js
+++ pages/profile/profilePost.js
@@ -1,26 +1,6 @@
 module.exports = {
-	api_get_player: function(req, res, dao){
-		var overwatch = require('overwatch-api');
-		const battletag = req.body.battletag.replace('#', '-').replace(' ', '');
-		if(battletag == ''){
-			res.set("Content-Type", "application/json");
-			res.send(req.body);
-			return;
-		}
-		overwatch.getProfile('pc', 'eu', battletag, (err, json) => {
-	  		if (!err)
-	  		{
-	  			req.body.stats = json;
-	  			/*req.body.stats.level = json.level || '-';
-	  			req.body.stats.games.quickplay.won = json.level || '-';
-	  			req.body.stats.competitive.rank = json.competitive.rank || '-';
-	  			req.body.stats.games.competitive.played = json.games.competitive.played || '-';
-	  			req.body.stats.games.competitive.won = json.games.competitive.won || '-';
-	  			req.body.stats.games.competitive.lost = json.games.competitive.lost || '-';
-	  			req.body.stats.playtime.quickplay = json.playtime.quickplay || '-';*/
-	  		}
-	  		res.set("Content-Type", "application/json");
-			res.send(req.body);
-		});
+	get_user : function(req, res, dao){
+		res.set("Content-Type", "application/json");
+		res.send(req.body.session);
 	}
 }
